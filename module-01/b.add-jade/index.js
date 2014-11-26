0d764af1d65c8db254d8b9111cc9cd6a95fad454
Damian Suarez <rdsuarez@gmail.com>
2014-11-26 06:45:19 -0300

    module-01: (b) step

---
+++ module-01/b.add-jade/index.js
@@ -0,0 +1,24 @@
+
+/**
+ * Modules dependencies
+ */
+
+var express = require('express');
+var app = express();
+
+// jade templates folder
+app.set('views', __dirname + '/views');
+
+// set jade rendering engine
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
